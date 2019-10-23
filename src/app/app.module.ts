cff05bf51eca9610e9ffdadb3c394fd59a4d51a0
Ian Sherwood <ian.r.sherwood@gmail.com>
2019-10-23 18:59:14 -0400

    First home page mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// TODO: We're importing WAY MORE than we need to here.
+// Later we can tree - shake this and thin it down
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatIconModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatProgressBarModule
+} from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +26,15 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
